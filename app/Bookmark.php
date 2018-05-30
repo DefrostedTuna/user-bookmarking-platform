e6b77aaac6eacb41f9d36e1276e3f1ca027f4c35
Rick Bennett <rbennett1106@gmail.com>
2018-05-29 20:15:35 -0400

    Added bookmark controller, migration, model, as well as route

---
+++ app/Bookmark.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bookmark extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'url',
+    ];
+
+    /**
+     * Returns the user which the bookmark is owned by.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
